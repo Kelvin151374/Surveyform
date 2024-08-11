6fc94a03c2d35de635ac1a1be70f2e6647a1e2c8
Kelvin151374 <khantkyawza38@gmail.com>
2024-08-11 13:06:12 +0630

    adding script.js style.css survey.html

---
+++ script.js
@@ -0,0 +1,23 @@
+// console.log("hello world");
+
+// var name;
+// let name;
+// const names;
+
+// {
+//     // let name;
+//     name = ''
+// }
+
+
+var num = 1;
+var name = "Kelvin";
+var bool = true;
+var nm = null;
+var one;
+
+console.log(typeof num);
+console.log(typeof name);
+console.log(typeof bool);
+console.log(typeof nm);
+console.log(typeof one);
